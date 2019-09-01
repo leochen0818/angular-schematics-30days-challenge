07d152af93ec63032efde5a8d079192644e368e4
Leo Chen <a0952250818@gmail.com>
2019-09-01 21:52:11 +0800

    完成支援在 Angular 專案中使用的功能

--- src/hello-world/index.ts
+++ src/hello-world/index.ts
@@ -1,19 +1,34 @@
-import { Rule, SchematicContext, Tree, url, apply, template, mergeWith } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, url, apply, template, mergeWith, SchematicsException, move } from '@angular-devkit/schematics';
 
 import { strings } from '@angular-devkit/core';
+import { parseName } from '@schematics/angular/utility/parse-name';
+import { buildDefaultPath } from '@schematics/angular/utility/project';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function helloWorld(_options: any): Rule {
-  return (_tree: Tree, _context: SchematicContext) => {
+export function helloWorld(_options: HelloWorldSchema): Rule {
+  return async (_tree: Tree, _context: SchematicContext) => {
 
+    const workspaceConfigBuffer = _tree.read('angular.json');
+    if ( !workspaceConfigBuffer ) {
+      throw new SchematicsException('Not an Angular CLI workspace');
+    }
+
+    const workspaceConfig = JSON.parse(workspaceConfigBuffer.toString());
+    const projectName = _options.project || workspaceConfig.defaultProject;
+    const project = workspaceConfig.projects[projectName];
+    const defaultProjectPath = buildDefaultPath(project);
+    const parsePath = parseName(defaultProjectPath, _options.name);
+    const { name, path } = parsePath;
+    
     const sourceTemplates = url('./files');
-
     const sourceParametrizedTemplates = apply(sourceTemplates, [
       template({
         ..._options,
-        ...strings
-      })
+        ...strings,
+        name
+      }),
+      move(path)
     ]);
 
     return mergeWith(sourceParametrizedTemplates);
