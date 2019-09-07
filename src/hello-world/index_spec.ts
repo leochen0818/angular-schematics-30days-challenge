869b8cbb0f3beeac141958b4726dbe1dbd3d6ec6
Leo Chen <a0952250818@gmail.com>
2019-09-07 20:31:33 +0800

    完成第一個測試

--- src/hello-world/index_spec.ts
+++ src/hello-world/index_spec.ts
@@ -2,15 +2,22 @@ import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
-
 const collectionPath = path.join(__dirname, '../collection.json');
 
-
 describe('hello-world', () => {
-  it('works', () => {
+  const expectResult = (fileName?: string) => {
+    const fullFileName = `/${fileName || 'hello'}`;
+    const params = fileName ? { name: fileName } : {};
     const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = runner.runSchematic('hello-world', {}, Tree.empty());
+    const tree = runner.runSchematic('hello-world', params, Tree.empty())
+    expect(tree.files).toContain(fullFileName);
+    expect(tree.readContent(fullFileName)).toEqual('world');
+  }
 
-    expect(tree.files).toEqual([]);
+  it('使用者沒給檔名，則檔名為 "/hello"，檔案內容為 "world"', () => {
+    expectResult();
+  });
+  it('使用者有給檔名，則檔名為使用者給的檔名，檔案內容為 "world"', () => {
+    expectResult('Leo');
   });
 });
