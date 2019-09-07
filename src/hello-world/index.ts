e72e841c02abfbf99fc20aaf02d92397839adf8c
Leo Chen <a0952250818@gmail.com>
2019-09-07 20:31:00 +0800

    完成第一個 Schematic

--- src/hello-world/index.ts
+++ src/hello-world/index.ts
@@ -5,6 +5,7 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 // per file.
 export function helloWorld(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    tree.create(_options.name || 'hello', 'world');
     return tree;
   };
 }
