a54f500d3997f68fbc2c3e670c5152fa49cfe7fa
Leo Chen <a0952250818@gmail.com>
2019-09-09 23:03:30 +0800

    完成使用範本的 Schematic 的測試

--- src/hello-world/index_spec.ts
+++ src/hello-world/index_spec.ts
@@ -1,23 +1,22 @@
 import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import { strings } from '@angular-devkit/core';
 import * as path from 'path';
 
 const collectionPath = path.join(__dirname, '../collection.json');
 
 describe('hello-world', () => {
-  const expectResult = (fileName?: string) => {
-    const fullFileName = `/${fileName || 'hello'}`;
-    const params = fileName ? { name: fileName } : {};
+  it('成功產出檔案，則檔名為/hello-leo-chen.component.ts', () => {
+    const name = 'LeoChen';
     const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = runner.runSchematic('hello-world', params, Tree.empty())
-    expect(tree.files).toContain(fullFileName);
-    expect(tree.readContent(fullFileName)).toEqual('world');
-  }
+    const tree = runner.runSchematic('hello-world', { name: name }, Tree.empty())
 
-  it('使用者沒給檔名，則檔名為 "/hello"，檔案內容為 "world"', () => {
-    expectResult();
-  });
-  it('使用者有給檔名，則檔名為使用者給的檔名，檔案內容為 "world"', () => {
-    expectResult('Leo');
+    const dasherizeName = strings.dasherize(name);
+    const fullFileName = `/hello-${dasherizeName}.component.ts`;
+    expect(tree.files).toContain(fullFileName);
+    
+    const fileContent = tree.readContent(fullFileName);
+    expect(fileContent).toMatch(/hello-leo-chen/);
+    expect(fileContent).toMatch(/HelloLeoChenComponent/);
   });
 });
