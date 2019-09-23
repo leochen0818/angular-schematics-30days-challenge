77630a27072427830da5240d7c11a6ab4eb259e6
Leo Chen <a0952250818@gmail.com>
2019-09-23 23:48:10 +0800

    完成使用 Angular Schematics API 重構

--- src/hello-world/index.ts
+++ src/hello-world/index.ts
@@ -3,8 +3,12 @@ import { Rule, SchematicContext, Tree, url, apply, template, mergeWith, Schemati
 import { strings } from '@angular-devkit/core';
 import { parseName } from '@schematics/angular/utility/parse-name';
 import { buildDefaultPath } from '@schematics/angular/utility/project';
+import { validateName, validateHtmlSelector } from '@schematics/angular/utility/validation';
+import { buildRelativePath, findModuleFromOptions, ModuleOptions } from '@schematics/angular/utility/find-module';
+import { addDeclarationToModule } from '@schematics/angular/utility/ast-utils';
+import { InsertChange } from '@schematics/angular/utility/change';
 
-import * as ts from 'typescript';
+import * as ts from '@schematics/angular/third_party/github.com/Microsoft/TypeScript/lib/typescript';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
@@ -23,56 +27,29 @@ export function helloWorld(_options: HelloWorldSchema): Rule {
     const parsePath = parseName(defaultProjectPath, _options.name);
     const { name, path } = parsePath;
     
-    let importPath = '';
-    if ( path === defaultProjectPath ) {
-      importPath = './';
-    } else {
-      importPath = path.replace(defaultProjectPath, '.') + '/';
-    }
-    importPath += 'hello-' + strings.dasherize(name) + '.component';
+    validateName(name);
+    validateHtmlSelector(`hello-${strings.dasherize(name)}`);
     
-    const appModulePath = `.${defaultProjectPath}/app.module.ts`;
-    const text = _tree.read(appModulePath) || [];
-    
-    let sourceFile = ts.createSourceFile(
+    const moduleOptions: ModuleOptions = { name: name, path: path };
+    const modulePath = findModuleFromOptions(_tree, moduleOptions) || '';
+    const sourceFile = ts.createSourceFile(
       'test.ts',
-      text.toString(),
+      (_tree.read(modulePath) || []).toString(),
       ts.ScriptTarget.Latest,
       true
     );
 
-    const classDeclaration = sourceFile.statements.find( node => ts.isClassDeclaration(node) )! as ts.ClassDeclaration;
-    const decorator = classDeclaration.decorators![0] as ts.Decorator;
-    const callExpression = decorator.expression as ts.CallExpression;
-    const objectLiteralExpression = callExpression.arguments[0] as ts.ObjectLiteralExpression;
-    const propertyAssignment = objectLiteralExpression.properties.find((property: ts.PropertyAssignment) => {
-      return (property.name as ts.Identifier).text === 'declarations'
-    })! as ts.PropertyAssignment;
-    const arrayLiteralExpression = propertyAssignment.initializer as ts.ArrayLiteralExpression;
-    const identifier = arrayLiteralExpression.elements[0] as ts.Identifier;
+    const componentPath = `${path}/hello-${strings.dasherize(name)}.component`;
+    const classifiedName = `Hello${strings.classify(name)}Component`;
+    const relativePath = buildRelativePath(modulePath, componentPath);
+    const declarationChanges = addDeclarationToModule(sourceFile, modulePath, classifiedName, relativePath);
 
-    const declarationRecorder = _tree.beginUpdate(appModulePath);
-    const changeText = identifier.getFullText(sourceFile);
-    const classifyName = strings.classify(name);
-    const componentName = `Hello${classifyName}Component`;
-    let toInsert = '';
-    if (changeText.match(/^\r?\r?\n/)) {
-      toInsert = `,${changeText.match(/^\r?\n\s*/)![0]}${componentName}`;
-    } else {
-      toInsert = `, ${componentName}`;
-    }
-    declarationRecorder.insertLeft(identifier.end, toInsert);
-
-    const allImports = sourceFile.statements.filter( node => ts.isImportDeclaration(node) )! as ts.ImportDeclaration[];
-    let lastImport: ts.Node | undefined;
-    for (const importNode of allImports) {
-      if ( !lastImport || importNode.getStart() > lastImport.getStart() ) {
-        lastImport = importNode;
+    const declarationRecorder = _tree.beginUpdate(modulePath);
+    for (const change of declarationChanges) {
+      if (change instanceof InsertChange) {
+        declarationRecorder.insertLeft(change.pos, change.toAdd);
       }
     }
-    const importStr = `\nimport { ${componentName} } from '${importPath}';`;
-    declarationRecorder.insertLeft(lastImport!.end, importStr);
-
     _tree.commitUpdate(declarationRecorder);
     
     const sourceTemplates = url('./files');
